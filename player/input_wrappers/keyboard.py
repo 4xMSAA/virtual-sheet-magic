4262b9f9deee4649c2a8522afd651ac5b44bc1c9
Aramix Asd <aramix273@gmail.com>
2021-08-11 20:54:13 +0300

    KeyboardWrapper implementation

--- player/input_wrappers/keyboard.py
+++ player/input_wrappers/keyboard.py
@@ -18,8 +18,23 @@ class KeyboardWrapper(BaseInputWrapper):
             ")": "shift+0",
         }
 
-    def send_key(self, key):
-        pass
+    def __solve_code(self, key):
+        if len(key) != 1:
+            raise Exception("Invalid key length")
 
-    def send_chord(self, key):
-        pass
+        if key in self.__specialUpperKeys:
+            return self.__specialUpperKeys[key]
+        elif key.isalnum():
+            if key.isupper():
+                code = "shift+" + key.lower()
+                return code
+            else:
+                return key
+
+    def send_key(self, key):
+        code = self.__solve_code(key)
+        keyboard.send(code)
+
+    def send_chord(self, chord):
+        for key in chord:
+            self.send_key(key)
