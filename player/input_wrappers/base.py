0e874403874658555eec8ef19a740117ad9586a5
Mia Soomre <27443298+4xMSAA@users.noreply.github.com>
2021-08-11 19:29:52 +0300

    add foundation for input wrapping

---
+++ player/input_wrappers/base.py
@@ -0,0 +1,6 @@
+class BaseInputWrapper():
+    def send_chord(self, chord):
+        pass
+    
+    def send_key(self, key):
+        pass
---
+++ player/input_wrappers/pynput.py
@@ -0,0 +1,9 @@
+from .base import BaseInputWrapper
+from pynput import keyboard
+
+class PynputWrapper(BaseInputWrapper):
+    def send_chord(self, chord):
+        pass
+
+    def send_key(self, key):
+        pass
