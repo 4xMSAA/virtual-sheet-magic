120b83d0f43bef8bdfa33fb73dbb01a8be7b09dd
Mia Soomre <27443298+4xMSAA@users.noreply.github.com>
2021-08-13 12:38:59 +0300

    obliterate test.py - run them individually or let IDE handle

--- tests/test_note.py
+++ tests/test_note.py
@@ -1,10 +1,16 @@
 import unittest
 from core.note import Note
 
+key_map = list("1!2@34$5%6^78*9(0qQwWeErtTyYuiIoOpPasSdDfgGhHjJklLzZxcCvVbBnm")
+midi_index_start = 12 # C1
 class TestNote(unittest.TestCase):
 
+    def test_key_to_note(self):
+        for key in key_map:
+            note = Note(from_key=key)
+            self.assertEqual(note.index, key_map.index(key) + midi_index_start)
+
     def test_note_to_key(self):
-        key_map = list("1!2@34$5%6^78*9(0qQwWeErtTyYuiIoOpPasSdDfgGhHjJklLzZxcCvVbBnm")
         for key in key_map:
             note = Note(from_key=key)
             self.assertEqual(key, note.to_key(0))
