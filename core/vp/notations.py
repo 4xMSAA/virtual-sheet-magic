4e0f8c2533c4fc231c4b55b1b0a67e92d02d9b8f
Mia Soomre <27443298+4xMSAA@users.noreply.github.com>
2021-08-28 18:06:08 +0300

    add pre-commit hooks and add note values to notations

--- core/vp/notations.py
+++ core/vp/notations.py
@@ -1,7 +1,4 @@
-"""
-Somewhat arbitrary notations from the varied amount of
-virtual piano music sheets seen online.
-"""
+"""Somewhat arbitrary notations from the varied amount of virtual piano music sheets seen online."""
 
 # notes that indicate pausing
 PAUSE = {
@@ -23,8 +20,19 @@ PAUSE = {
     }
 }
 
+# used for more precise control over note values. e.g: every < splits further by 2
+# more examples:
+# <[qwerty]<[qwerty] -> play [qwerty][qwerty] by
+#                       1/16 1/16 rhythmic value instead of 1/8 1/8
+# q>w>e>r>t>y> -> play every note as half note rather than quarter note (1/2, 1/4)
+NOTE_VALUES = {
+    "less": {"symbol": "<"}
+    "more": {"symbol": ">"}
+}
+
 # notes which are played simultaneously
 CHORDS = {"begin": {"symbol": "["}, "end": {"symbol": "]"}}
 
 # notes which are played VERY FAST but not simultaneously
+# assumably, this is used with 1/16 rhyhtmic values
 BROKEN_CHORDS = {"begin": {"symbol": "{"}, "end": {"symbol": "}"}}
