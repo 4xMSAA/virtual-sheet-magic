32bf3660abab6d0814a6bb5e6b53ea48acfd878b
Mia Soomre <27443298+4xMSAA@users.noreply.github.com>
2021-10-20 14:57:40 +0300

    tidy up sending input a bit

--- player/input_wrappers/base.py
+++ player/input_wrappers/base.py
@@ -1,4 +1,40 @@
+from core import Chord
+
+MINOR_KEYS = {
+    "!": "1",
+    "@": "2",
+    "$": "4",
+    "%": "5",
+    "^": "6",
+    "*": "8",
+    "(": "9",
+    "Q": "q",
+    "W": "w",
+    "E": "e",
+    "T": "t",
+    "Y": "y",
+    "I": "i",
+    "O": "o",
+    "P": "p",
+    "S": "s",
+    "D": "d",
+    "G": "g",
+    "H": "h",
+    "J": "j",
+    "L": "l",
+    "Z": "z",
+    "C": "c",
+    "V": "v",
+    "B": "b",
+}
+
+
 class BaseInputWrapper():
+    def has_minor_keys(chord: Chord) -> bool:
+        for note in chord:
+            if note.to_key() in MINOR_KEYS:
+                return True
+
     def send_chord(self, chord):
         raise NotImplementedError()
 
--- player/input_wrappers/pynput.py
+++ player/input_wrappers/pynput.py
@@ -1,41 +1,7 @@
 from pynput import keyboard
 from core import Note
 from core import Chord
-from .base import BaseInputWrapper
-
-MINOR_KEYS = {
-    "!": "1",
-    "@": "2",
-    "$": "4",
-    "%": "5",
-    "^": "6",
-    "*": "8",
-    "(": "9",
-    "Q": "q",
-    "W": "w",
-    "E": "e",
-    "T": "t",
-    "Y": "y",
-    "I": "i",
-    "O": "o",
-    "P": "p",
-    "S": "s",
-    "D": "d",
-    "G": "g",
-    "H": "h",
-    "J": "j",
-    "L": "l",
-    "Z": "z",
-    "C": "c",
-    "V": "v",
-    "B": "b",
-}
-
-
-def has_minor_keys(chord: Chord) -> bool:
-    for note in chord:
-        if note.to_key() in MINOR_KEYS:
-            return True
+from .base import BaseInputWrapper, MINOR_KEYS
 
 
 class PynputWrapper(BaseInputWrapper):
@@ -43,19 +9,20 @@ class PynputWrapper(BaseInputWrapper):
         self.keyboard = keyboard.Controller()
 
     def send_chord(self, chord: Chord):
-        if has_minor_keys(chord):
+        if self.has_minor_keys(chord):
             self.keyboard.press(keyboard.Key.shift)
+
             for note in chord:
                 key = note.to_key()
                 if key in MINOR_KEYS:
                     self.keyboard.type(MINOR_KEYS[key])
+
             self.keyboard.release(keyboard.Key.shift)
 
         for note in chord:
             key = note.to_key()
-            if not key in MINOR_KEYS:
+            if key not in MINOR_KEYS:
                 self.keyboard.type(key)
-
 
     def send_note(self, note: Note):
         key = note.to_key()
